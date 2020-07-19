e020a2f8796a425395fd17e8062f64e006119f0b
Johny-tech <limonsoft.rustam05@gmail.com>
2020-07-20 00:02:46 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# React-recipe-App
+
+
+A web app to fetch different recpies from around the globe
+
+How to use :-
+
+for developers
+
+1> Clone the repo
+<br>
+2> npm install
+<br>
+3> npm start
+<br>
+4> Open the app.js and enjoy
+
+for non-developers
+
+click here
+<a href="https://recipies.daggron.now.sh/">Here</a>
---
+++ build/precache-manifest.8bbaf889b2f7ffc5c2f2137616de6ed0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d20d02e0508d44ec721aa0dfa5f8b0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c6da9d20b08f911de2d",
+    "url": "/static/css/main.cf902f3c.chunk.css"
+  },
+  {
+    "revision": "b55e12cc8e6278c165ca",
+    "url": "/static/js/2.7480d6c3.chunk.js"
+  },
+  {
+    "revision": "3c6da9d20b08f911de2d",
+    "url": "/static/js/main.c0051771.chunk.js"
+  },
+  {
+    "revision": "5b04a122f23a80686fcc",
+    "url": "/static/js/runtime~main.4a0fe304.js"
+  }
+]);
